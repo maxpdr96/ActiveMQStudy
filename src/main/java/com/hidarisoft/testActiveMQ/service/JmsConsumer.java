44ffd231db700c4f84287cdb5b56d2e88c84eaad
maxpdr96 <maxpdr96@gmail.com>
2021-04-24 14:44:57 -0300

    First Commit

--- src/main/java/com/hidarisoft/testActiveMQ/service/JmsConsumer.java
+++ src/main/java/com/hidarisoft/testActiveMQ/service/JmsConsumer.java
@@ -2,10 +2,6 @@ package com.hidarisoft.testActiveMQ.service;
 
 import com.hidarisoft.testActiveMQ.model.Employee;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.dirigible.commons.api.helpers.FileSystemUtils;
-import org.eclipse.dirigible.commons.api.helpers.GsonHelper;
-import org.eclipse.dirigible.commons.api.module.DirigibleModulesInstallerModule;
-import org.eclipse.dirigible.database.h2.H2Database;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -24,7 +20,6 @@ public class JmsConsumer implements MessageListener {
         try{
             ObjectMessage objectMessage = (ObjectMessage)message;
             Employee employee = (Employee)objectMessage.getObject();
-            FileSystemUtils.createFile("test");
 
             log.info("Received Message: "+ employee.toString());
         } catch(Exception e) {
